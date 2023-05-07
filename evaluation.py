1d0710ad55d5d40ab7591cb661de2327633e31d7
Maxence B <maxence.barre59@gmail.com>
2023-05-07 19:29:00 +0200

    ajout d'une fonction qui détermine si un problème est résoluble ou non la démonstration de cette fonction n'est pas encore faite mais l'hypothèse semble valide qql autres ajouts

--- evaluation.py
+++ evaluation.py
@@ -3,12 +3,14 @@
 # importation des modules
 import random as rd
 import main as m
+import time as t
+import numpy as np
 
 
 # définition des variables initiales
-nb_eleves_max = 400
-nb_contraintes_max = 50
-nb_chambres_max = 100
+nb_eleves_max = 100
+nb_contraintes_max = 10
+nb_chambres_max = 10
 
 
 # création des fonctions initiales
@@ -17,7 +19,7 @@ def generation_incompatibilite(nb_contraintes, nb_eleves):
     je crois que pour n élèves il y a au plus n(n+1)/2 contraintes possibles
     hypothèse établie sur les premiers entiers mais non vérifiée
     """
-    assert nb_contraintes <= int(nb_eleves(nb_eleves+1)/2)
+    assert nb_contraintes <= int(nb_eleves*(nb_eleves-1)/2)
     retour = list()
     while len(retour) < nb_contraintes:
         contrainte = rd.sample(range(nb_eleves), 2)
@@ -27,6 +29,9 @@ def generation_incompatibilite(nb_contraintes, nb_eleves):
     return retour
 
 def verification(proposition, contraintes):
+    # print(proposition)
+    if type(proposition) == str:
+        return proposition
     for elt in contraintes:
         if not(elt[0] in proposition and elt[1] in proposition):
             pass  # tout va bien
@@ -35,19 +40,79 @@ def verification(proposition, contraintes):
             print(f"contraintes imposées: {contraintes}")
             print(f"proposition: {proposition}")
             return "ERREUR_ALGO_NON_VALIDE"
-        
+    return "PROPOSITION_VALIDE"
+
+def count_zero_par_ligne(matrice):
+    """
+    >>> count_zero_par_ligne(np.array([[1, 1, 2], [0, 0, 0], [1, 0, 1]]))
+    [0, 3, 1]
+    """
+    retour = [np.count_nonzero(matrice[i] == 0) for i in range(len(matrice))]
+    return retour
+
+def test_insolubilite(contrainte, nb_eleves, nb_chambres):
+    """
+    fonction non vérifiée, basée sur une hypothèse établie sur les premiers entiers
+    je pense que cela permetterait d'ailleurs d'offrir un nouvel algorithme possible
+    pour l'instant, je pense que:
+        test_insolubilite() is True => problème insoluble
+        attention reciproque fausse pour l'instant!
+    hypothèse:
+        en notant n=nb_chambre et A la matrice-contrainte alors:
+        le problème est résoluble ssi il existe au moins n lignes de A^n comportant n-1 coeff égaux à 0
+    >>> test_insolubilite([(1, 4), (3, 4), (0, 1), (0, 6), (0, 4), (1, 6)], 7, 5)
+    True
+    >>> test_insolubilite([(0, 2)], 3, 3)
+    True
+    """
+    print(contrainte)
+    matrice = m.generation_matrice_contrainte(contrainte, nb_eleves)
+    # print(matrice)
+    power = np.linalg.matrix_power(matrice, nb_chambres)
+    # print(power)
+    indic = power[power==0]
+    transfo = np.count_nonzero(np.array(count_zero_par_ligne(power)) >= nb_chambres - 1)
+    return transfo < nb_chambres  # True si insoluble
 
 # fonction pour tester la robustesse de l'algorithme
 def test(nb_test):
     for _ in range(nb_test):
         nb_eleves = rd.randint(2, nb_eleves_max)
-        nb_contraintes = rd.randint(1, min(int(nb_eleves(nb_eleves+1)/2), nb_contraintes_max))
+        nb_contraintes = rd.randint(1, min(int(nb_eleves*(nb_eleves-1)/2), nb_contraintes_max))
         nb_chambres = rd.randint(1, min(nb_chambres_max, nb_eleves))
-        contraintes = generation_incompatibilite(nb_contraintes)
+        print(f"\n {nb_contraintes} {nb_eleves} {nb_chambres}")
+        contraintes = generation_incompatibilite(nb_contraintes, nb_eleves)
+        print(contraintes)
         retour = m.main(contraintes, nb_eleves, nb_chambres)
+        if retour == None:
+            print("none", contraintes, nb_eleves, nb_chambres)
+        if retour == "INSOLUBLE":
+            return "INSOLUBLE"
         if retour == "ERREUR_GENERATION":
-            return
+            return "ERREUR_GENERATION"
         if verification(retour, contraintes) == "ERREUR_ALGO_NON_VALIDE":
             print(nb_eleves, nb_chambres, nb_contraintes)
             return
+        print("\n")
         
+def test_clay(nb_contraintes, verbose=True):
+    debut = t.time()
+    contraintes = generation_incompatibilite(nb_contraintes, 400)
+    mil = t.time()
+    retour = m.main(contraintes, 400, 100)
+    fin = t.time()
+    check=verification(retour, contraintes)
+    if verbose:
+        print(retour)
+        print(check)
+        print(f"gen_contrainte: {mil-debut}, main: {fin-mil}, verif: {t.time()-fin}")
+    return check
+
+
+def test_clay_puissant(nb_max_contraintes, nb_test_par_contrainte):
+    for nb_contrainte in range(1, nb_max_contraintes):
+        indic = 0
+        for _ in range(nb_test_par_contrainte):
+            if test_clay(nb_contrainte, False) == "INSOLUBLE":
+                indic += 1
+        print(nb_contrainte, indic)
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def score_eleves_chiants(contrainte, nb_eleves):
     permet de mesurer la "chiantise" d'un élève, plus un élève est incompatible avec les autres
     plus il est considéré comme chiant (et donc sera moins prioritaire à l'internat)
     """
-    retour = [(i, 0) for i in range(nb_eleves)]  # (num_eleve, score_chiant)
+    retour = [[i, 0] for i in range(nb_eleves)]  # (num_eleve, score_chiant)
     for elt in contrainte:
         retour[elt[0]][1] += 1
         retour[elt[1]][1] += 1
@@ -44,15 +44,25 @@ def generation_matrice_contrainte(contraintes, nb_eleves):
 
 # fonction principale
 def main(contraintes, nb_eleves, nb_chambres):
+    """
+    >>> main([(0, 2)], 3, 2)
+    [1, 2]
+    >>> main([(1, 4), (3, 4), (0, 1), (0, 6), (0, 4), (1, 6)], 7, 5)  # problème insoluble
+    problème insoluble
+    'INSOLUBLE'
+    >>> main([(1, 2), (1, 5), (0, 3), (0, 7), (0, 1), (1, 7), (6, 7), (0, 6), (1, 4), (2, 4)], 8, 8)
+    problème insoluble
+    'INSOLUBLE'
+    """
     chiantise = score_eleves_chiants(contraintes, nb_eleves)
     eleves_pas_chiants, eleves_chiants = separer_chiants_ou_non(chiantise, nb_eleves)
-
     # cas 1: il y a plus d'élèves non-chiants que de places disponibles
     if len(eleves_pas_chiants) >= nb_chambres:
-        retour = rd.sample(eleves_pas_chiants, nb_chambres)
+        # retour = rd.sample(eleves_pas_chiants, nb_chambres)  # on enlève pour l'instant le choix aléatoire afin de faciliter les vérifications à la main
+        retour = eleves_pas_chiants[:nb_chambres]
         return retour
-
-    elif len(eleves_pas_chiants) > 0:
+    
+    elif len(eleves_pas_chiants) >= 0:
         retour = eleves_pas_chiants.copy()
         # il faut maintenant caser les élèves chiants
         matrice_contrainte = generation_matrice_contrainte(contraintes, nb_eleves)
@@ -60,7 +70,10 @@ def main(contraintes, nb_eleves, nb_chambres):
         eleves_dispo = [eleves_chiants[i][0] for i in range(len(eleves_chiants))]
         chambres_restantes = nb_chambres - len(retour)
         while chambres_restantes > 0:
-            if len(eleve_a_placer) == 0 or len(classement) == 0:
+            if len(eleves_dispo) < chambres_restantes:
+                print("problème insoluble")
+                return "INSOLUBLE"
+            if len(eleves_dispo) == 0 or len(classement) == 0:
                 print("ERREUR: L'algorithme ne parvient pas à conclure!!!")
                 print(f"nb_élèves: {nb_eleves}, nb_chambres: {nb_chambres}, nb_contraintes: {len(contraintes)}")
                 print(f"contraintes:\n {contraintes}")
@@ -69,8 +82,13 @@ def main(contraintes, nb_eleves, nb_chambres):
             if eleve_a_placer not in eleves_dispo:
                 continue
             retour.append(eleve_a_placer)
+            chambres_restantes -= 1
             for i in range(len(matrice_contrainte)):
-                if matrice_contrainte[eleve_a_placer][i] == 1:
+                if matrice_contrainte[eleve_a_placer][i] == 1 and i in eleves_dispo:
                     eleves_dispo.remove(i)
         return retour
         
+if __name__ == "__main__" and True:
+    import doctest
+    doctest.testmod(verbose=False)
+    print("done")
